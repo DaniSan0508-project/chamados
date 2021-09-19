22b271a9f64cf0bcc5a0e7a54abedf02607c578c
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-19 15:19:45 -0300

    fix: corrigido importaçao da rota Dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@
 
     import SignIn from '../pages/SignIn';
     import SignUp from '../pages/SignUp';
-    import Dashboard from '../Dashboard';
+    import Dashboard from '../pages/Dashboard';
 
     export default function Routes(){
         return(
             <Switch>
                 <Route component={SignIn} path="/" exact/>
                 <Route component={SignUp} path="/register" exact/>
-                <Route component={Dashboard} path="/dashboard" exact isPrivate/>
+                <Route component={Dashboard} path="/dashboard" exact isPrivate  />
             </Switch>
         )
     }
