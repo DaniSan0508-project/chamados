999f46f174c3956a778fef2a14898d89cfdff834
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-20 15:29:28 -0300

    add componente de menu e responsividade

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,10 +2,13 @@
 import { useContext } from 'react/cjs/react.development';
 import { AuthContext } from '../../contexts/auth';
 
+import Header from '../../components/Header';
+
 export default function Dashboard(){
     const { signOut } = useContext(AuthContext);
     return(
         <div>
+            <Header/>
             <h1>Tela do dashboard</h1>
             <button onClick={()=> signOut()}>Logout</button>
         </div>
