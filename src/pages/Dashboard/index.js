a4680f281156bb557278536715597ef3eb20b58a
Daniel <danielsantoscarneiro0508@gmail.com>
2021-09-21 16:30:51 -0300

    add estilizacao da tabela do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,17 +5,17 @@ import { AuthContext } from '../../contexts/auth';
 import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
-import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 export default function Dashboard(){
-    const [chamados, setChamados] = useState([]);
+    const [chamados, setChamados] = useState([1]);
 
     return(
         <div>
             <Header/>
             <div className="content">
                 <Title name="Atendimentos">
-                    <FiMessageSquare size={25}/>;
+                    <FiMessageSquare size={25}/>
                 </Title>
                 {
                     chamados.length === 0 ?
@@ -36,6 +36,36 @@ export default function Dashboard(){
                             <FiPlus size={25} color="#fff"/>
                             Novo Chamado
                         </Link>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Cadastrado em</th>
+                                    <th scope="col">#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td data-label="Cliente">Sujeito</td>
+                                    <td data-label="Cliente">Suporte</td>
+                                    <td data-label="Status">
+                                        <span className="badge" style={{backgroundColor: '#5cb85c'}}>Em Aberto</span>
+                                    </td>
+                                    <td data-label="Cadastrado">20/06/2021</td>
+                                    <td data-label="#">
+                                        <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                            <FiSearch color="#fff" size={17}/>
+                                        </button>
+                                        <button className="action" style={{backgroundColor: '#F6a935'}}>
+                                            <FiEdit2 color="#fff" size={17}/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </>
                     )
                 }
